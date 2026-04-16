097b7c6a734496119281f6e0883a1144816ef303
Maralmhz <maralmhz@gmail.com>
2026-04-16 15:33:21 -0300

    fix: atualiza cache para v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meudindin-v3';
+const CACHE = 'meudindin-v4';
 const ASSETS = ['/', '/index.html'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
