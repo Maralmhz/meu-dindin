47f5a3ad01be0277415a0f4b0876a19ee369edaa
Maralmhz <maralmhz@gmail.com>
2026-04-16 15:40:57 -0300

    fix: cache v5 para forçar atualização

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meudindin-v4';
+const CACHE = 'meudindin-v5';
 const ASSETS = ['/', '/index.html'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
