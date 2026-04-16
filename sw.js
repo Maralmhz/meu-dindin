71140ffcc028de014ad1018c49028209890395d6
Maralmhz <maralmhz@gmail.com>
2026-04-16 15:26:05 -0300

    fix: atualiza cache para v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meudindin-v2';
+const CACHE = 'meudindin-v3';
 const ASSETS = ['/', '/index.html'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
